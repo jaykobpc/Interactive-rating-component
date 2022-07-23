a99ad6c4e8373e96dc196fd7b3c02b9eaed5771f
jaykobpc <watenga.jacob@gmail.com>
2022-07-23 23:00:05 +0300

    form shake animation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const ratingForm = document.querySelector("#ratingForm");
 const ratingFeedback = document.querySelector("#ratingFeedback");
 const selectedValue = document.querySelector("#selectedValue");
+const Container = document.querySelector("#container");
 const Form = document.querySelector("form");
 
 let selectedValueState = 1; //default state
@@ -10,6 +11,15 @@ Form.addEventListener("submit", (evt) => {
 
   const outputValue = Form.elements["rating-btn"].value;
 
+  // add form shake animation
+  if (!outputValue) Container.classList.add("form-invalid");
+
+  // remove form shake animation
+  setTimeout(() => {
+    Container.classList.remove("form-invalid");
+  }, 1000);
+
+  // still check
   if (outputValue) {
     selectedValueState = outputValue;
     selectedValue.innerText = selectedValueState;
