cd235db098bb3f2769d421c31edaa72bbaf6c94a
jaykobpc <watenga.jacob@gmail.com>
2022-07-23 22:12:44 +0300

    Rating component

---
+++ index.js
@@ -0,0 +1,18 @@
+const ratingForm = document.querySelector("#ratingForm");
+const ratingFeedback = document.querySelector("#ratingFeedback");
+const selectedValue = document.querySelector("#selectedValue");
+const Form = document.querySelector("form");
+
+let selectedValueState = 1; //default state
+
+Form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  const outputValue = Form.elements["rating-btn"].value;
+  selectedValueState = outputValue;
+
+  selectedValue.innerText = selectedValueState;
+
+  ratingForm.classList.add('hide');
+  ratingFeedback.classList.add('show');
+});
